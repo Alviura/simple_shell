04555657f5c84d6e3243070a817c664e0053cfd7
root <alviura@outlook.com>
2023-09-08 11:27:32 -0700

    Passes betty stlye

--- write.c
+++ write.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point to shell program
  *
- * Description: the program writes a $ on stdout
+ * Description: the program writes a Hello world message on stdout
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char *dollar = "$ ";
+	char *dollar = "Hello World\n ";
 
-	write(STDOUT_FILENO, dollar, 2);
+	write(STDOUT_FILENO, dollar, 13);
 	return (0);
 }
