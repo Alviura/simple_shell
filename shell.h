b07460e2c117b9a90bf7ab656a1e3ee7e79784f7
root <alviura@outlook.com>
2023-09-12 13:14:32 -0700

    Update on the header files and macro def

--- shell.h
+++ shell.h
@@ -1,2 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <errno.h>
+
+/* for read/write buffers */
+#define READ_BUFF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+
+
